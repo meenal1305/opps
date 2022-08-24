f44227d9d712f1c64d746a98e10caaa472bb982b
meenal1305 <100236356+meenal1305@users.noreply.github.com>
2022-08-24 20:25:59 +0530

    Create linklist2.cpp

---
+++ linklist2.cpp
@@ -0,0 +1,37 @@
+class node{
+    public:
+    int data;
+    node* next;
+ node(int x)
+    {
+    data=x;
+    next=NULL;
+    }
+};
+int main() {
+    node* head;
+    node* tail;
+    bool first=true;
+    
+    int n=2;
+    while(n--)
+    {
+        int val; cin>>val;
+        
+        node* temp=new node(val);
+            
+        if(first==true)
+        {
+            head=temp;
+            tail=temp;
+            first=false;
+        }
+        else
+        {
+            tail->next=temp;
+                tail=temp;
+            
+        }
+        cout<<val<<endl;
+    }
+}
